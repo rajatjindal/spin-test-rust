6709908851edeb875daee7ae9e633870499f0232
Rajat Jindal <rajatjindal83@gmail.com>
2023-01-18 13:01:59 +0530

    add more tests

--- crates/e2e-testing/src/testcases.rs
+++ crates/e2e-testing/src/testcases.rs
@@ -4,7 +4,13 @@ use crate::testcase::TestCase;
 use anyhow::Result;
 
 pub fn all_testcases() -> Vec<TestCase> {
-    return vec![http_go_works(), http_c_works()];
+    return vec![
+        http_go_works(),
+        http_c_works(),
+        http_grain_works(),
+        http_rust_works(),
+        http_zig_works(),
+    ];
 }
 
 pub fn http_go_works() -> TestCase {
@@ -29,6 +35,45 @@ pub fn http_c_works() -> TestCase {
         name: "http-c template".to_string(),
         appname: "http-c-test".to_string(),
         template: Some("http-c".to_string()),
+        assertions: checks,
+    };
+}
+
+pub fn http_rust_works() -> TestCase {
+    fn checks(app: &App) -> Result<()> {
+        return assert_http_request(app.metadata.base.as_str(), 200, &[], "Hello, Fermyon");
+    }
+
+    return TestCase {
+        name: "http-rust-template".to_string(),
+        appname: "http-rust-test".to_string(),
+        template: Some("http-rust".to_string()),
+        assertions: checks,
+    };
+}
+
+pub fn http_zig_works() -> TestCase {
+    fn checks(app: &App) -> Result<()> {
+        return assert_http_request(app.metadata.base.as_str(), 200, &[], "Hello World!\n");
+    }
+
+    return TestCase {
+        name: "http-zig-template".to_string(),
+        appname: "http-zig-test".to_string(),
+        template: Some("http-zig".to_string()),
+        assertions: checks,
+    };
+}
+
+pub fn http_grain_works() -> TestCase {
+    fn checks(app: &App) -> Result<()> {
+        return assert_http_request(app.metadata.base.as_str(), 200, &[], "Hello, World\n");
+    }
+
+    return TestCase {
+        name: "http-grain-template".to_string(),
+        appname: "http-grain-test".to_string(),
+        template: Some("http-grain".to_string()),
         assertions: checks,
     };
 }
